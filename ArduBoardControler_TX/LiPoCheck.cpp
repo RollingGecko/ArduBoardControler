1f33bee4e7af3db1ca60bd3cda5b3eb41eb203bc
RollingGecko <andreas.chaitidis@gmail.com>
2017-10-29 13:35:44 +0100

    LiPoCheck: Added support fur multiple cell types and discharge curve for LiIon Cells

--- ArduBoardControler_TX/LiPoCheck.cpp
+++ ArduBoardControler_TX/LiPoCheck.cpp
@@ -18,48 +18,96 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 
 #include "LiPoCheck.h"
 
-int CountCells(float voltage) {
+int CountCells(float voltage, int celltype) {
 	
 	for (int i = 1; i <= MAXNUMBERCELLS; i++)
 	{
-		if ((voltage > ((float)i*MINVOLRANGE)) && (voltage < ((float)i*MAXVOLRANGE)))
+		if (celltype == CELL_LIPO)
 		{
-			return i;
+			if ((voltage > ((float)i*LIPO_MINVOLRANGE)) && (voltage < ((float)i*LIPO_MAXVOLRANGE)))
+			{
+				return i;
+			}
+		}
+		if (celltype == CELL_LIION)
+		{
+			if ((voltage > ((float)i*LIION_MINVOLRANGE)) && (voltage < ((float)i*LIION_MAXVOLRANGE)))
+			{
+				return i;
+			}
 		}
 	}
 	return 0;
 }
 
-int CapCheckPerc(float voltage, int cells) {
+int CapCheckPerc(float voltage, int cells, int celltype) {
 	float voltageCell = 0;
 	int ind = 0;
 
-	if (cells > 0)
+	switch (celltype)
 	{
-		voltageCell = (voltage / cells);
-	}
+	case 0:
+		if (cells > 0)
+		{
+			voltageCell = (voltage / cells);
+		}
 
-	if (voltageCell >=4.20)
-	{
-		return (100);
-	}
+		if (voltageCell >= LIPO_MAXVOLRANGE)
+		{
+			return (100);
+		}
 
-	while (!(voltageCell<=lipoDc[0][ind+1] && voltageCell > lipoDc[0][ind])&& ind<=10)
-	{
-		ind++;
-	}
-	
+		while (!(voltageCell <= lipoDc[0][ind + 1] && voltageCell > lipoDc[0][ind]) && ind <= 10)
+		{
+			ind++;
+		}
 
-	if (voltageCell <= lipoDc[0][ind + 1] && voltageCell > lipoDc[0][ind])
-	{
-		float CapacPers = (((lipoDc[1][ind + 1] - lipoDc[1][ind])/ (lipoDc[0][ind + 1] - lipoDc[0][ind]))*(voltageCell - lipoDc[0][ind])) + lipoDc[1][ind];
-	//	int intCapacPers = (int)(CapacPers * 100);
-		return (CapacPers * 100);
-	}
-	else
-	{
+		if (voltageCell <= lipoDc[0][ind + 1] && voltageCell > lipoDc[0][ind])
+		{
+			float CapacPers = (((lipoDc[1][ind + 1] - lipoDc[1][ind]) / (lipoDc[0][ind + 1] - lipoDc[0][ind]))*(voltageCell - lipoDc[0][ind])) + lipoDc[1][ind];
+			//	int intCapacPers = (int)(CapacPers * 100);
+			return (CapacPers * 100);
+		}
+		else
+		{
+			return 0;
+
+		}
+
+		break;
+	case 1:
+		if (cells > 0)
+		{
+			voltageCell = (voltage / cells);
+		}
+
+		if (voltageCell >= LIION_MAXVOLRANGE)
+		{
+			return (100);
+		}
+
+		while (!(voltageCell <= liionDc[0][ind + 1] && voltageCell > liionDc[0][ind]) && ind <= 10)
+		{
+			ind++;
+		}
+
+		if (voltageCell <= liionDc[0][ind + 1] && voltageCell > liionDc[0][ind])
+		{
+			float CapacPers = (((liionDc[1][ind + 1] - liionDc[1][ind]) / (liionDc[0][ind + 1] - liionDc[0][ind]))*(voltageCell - liionDc[0][ind])) + liionDc[1][ind];
+			//	int intCapacPers = (int)(CapacPers * 100);
+			return (CapacPers * 100);
+		}
+		else
+		{
+			return 0;
+
+		}
+
+		break;
+
+	default:
 		return 0;
-		
+		break;
 	}
 }
 
--- ArduBoardControler_TX/LiPoCheck.h
+++ ArduBoardControler_TX/LiPoCheck.h
@@ -33,11 +33,19 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 
 #endif // !_CONFIG_h
 
+#define CELL_LIPO	0
+#define LIPO_MINVOLRANGE	3.3
+#define	LIPO_MAXVOLRANGE	4.2
+#define CELL_LIION	1
+#define LIION_MINVOLRANGE	2.5
+#define	LIION_MAXVOLRANGE	4.2
+
 //const float lipoDc[2][8] = { { 0.0, 3.670,3.740,3.790,3.820,3.900,4.030,4.200 } , {0.000, 0.100,0.200,0.300,0.400,0.600,0.800,1.000 } };
 const float lipoDc[2][11] = { { 3.2, 3.680,3.740,3.780,3.810,3.850,3.870,3.960,4.02,4.100,4.200 } ,{ 0.000, 0.100,0.200,0.300,0.400,0.500,0.600,0.700,0.800,0.900,1.000 } };
-int CountCells(float voltage);
+const float liionDc[2][6] = { {3.0, 3.6, 3.68, 3.75, 3.88, 4.2} ,{ 0.000, 0.200,0.400,0.600,0.800, 1.000 } }; //source: http://www.ibt-power.com/bkencel/Graphs/LiIonDischGph.JPG
+int CountCells(float voltage, int celltype);
 
-int CapCheckPerc(float voltage, int cells);
+int CapCheckPerc(float voltage, int cells, int celltype);
 
 
 #endif
